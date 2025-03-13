b66c9a8668b02c8544eaffbe580fe805d9a50b45
Arthur <4rghplay@gmail.com>
2025-03-13 14:01:59 +0300

    git test commit+push

---
+++ src/main/java/com/javarush/kozlov/ConsoleRunner.java
@@ -0,0 +1,22 @@
+package com.javarush.kozlov;
+
+import java.util.Arrays;
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        String russian = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        String english = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String digit = "0123456789";
+        String symbols = "!@#$%^&*(){}[],.";
+        String alphabet = russian + english + russian.toLowerCase() +
+                english.toLowerCase() + digit + symbols;
+
+        int key = 123; // <<<< args
+        String text = "Привет медвед!"; // <<<< args
+        String result = "......"; // <<<< args
+
+
+
+        System.out.println(Arrays.toString(args));
+    }
+}
--- src/main/java/com/javarush/kozlov/CryptoRunner.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.kozlov;
-
-public class CryptoRunner {
-    public static void main(String[] args) {
-        System.out.println("I do it.");
-    }
-}
