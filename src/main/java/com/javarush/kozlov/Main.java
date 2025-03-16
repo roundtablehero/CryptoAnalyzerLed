65238035bd8669d9760b84e8f7c28ebe7087e1ca
Arthur <4rghplay@gmail.com>
2025-03-16 18:48:07 +0300

    ADD decrypt method

--- src/main/java/com/javarush/kozlov/Main.java
+++ src/main/java/com/javarush/kozlov/Main.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Main {
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '!', '?', ' '};
     private static final Map<Character, Integer> map = new HashMap<>();
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
 
     static {
@@ -43,12 +43,20 @@ public class Main {
         return sb.toString();
     }
 
+    private static String decrypt(String text, int key) { //<<< String to charArray
+        StringBuilder sb = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            sb.append(encryptChar(c, -key));
+        }
+        return sb.toString();
+    }
+
     private static String readFile(String filePath) {
         StringBuilder sb = new StringBuilder();
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8)) {
             String line;
             while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
+                sb.append(line).append("\n");
             }
         } catch (IOException e) {
             throw new RuntimeException("ERROR reading file: " + e.getMessage());
@@ -86,14 +94,18 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    int key = getKey();
-                    String text = readFile("text/text.txt");
-                    String encrypted = encrypt(text, key);
+                    int keyEncrypt = getKey();
+                    String textEncrypt = readFile("text/text.txt");
+                    String encrypted = encrypt(textEncrypt, keyEncrypt);
                     writeFile("text/text_encrypted.txt", encrypted);
                     System.out.println("Encrypted text saved to text/text_encrypted.txt");
                     break;
                 case 2:
-                    System.out.println("Decrypt WIP.");
+                    int keyDecrypt = getKey();
+                    String textDecrypt = readFile("text/text_encrypted.txt");
+                    String decrypted = decrypt(textDecrypt, keyDecrypt);
+                    writeFile("text/text_decrypted.txt", decrypted);
+                    System.out.println("Decrypted text saved to text/text_decrypted.txt");
                     break;
                 case 3:
                     System.out.println("Application closed.");
